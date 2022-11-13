626d19b30372f4fca490a846dad8a8e097b9a06a
decipher <dennis.heine@gmx.de>
2022-11-13 20:36:22 +0100

    fixed flickering, fixed height

--- Assets/Scripts/ControllerOverlay.cs
+++ Assets/Scripts/ControllerOverlay.cs
@@ -39,33 +39,36 @@ namespace EVRC
             System.Drawing.Font testFont = new System.Drawing.Font(System.Drawing.FontFamily.GenericMonospace, 400.0f, System.Drawing.FontStyle.Regular,
                                      GraphicsUnit.Pixel);
             texture = new Texture2D(2,2 );
-            ((Texture2D)texture).LoadImage(TxtToImage.DrawText(ButtonHandler.note, testFont, System.Drawing.Color.Orange, 3000,".\\"));
-
-            if (texture != null && handle != OpenVR.k_ulOverlayHandleInvalid)
+            if (ButtonHandler.note != GameObject.Find("txtNote").GetComponent<TMPro.TextMeshProUGUI>().text)
             {
-                var o = new Utils.OverlayHelper(handle);
-                o.Show();
+                ((Texture2D)texture).LoadImage(TxtToImage.DrawText(ButtonHandler.note, testFont, System.Drawing.Color.Orange, 3000, ".\\"));
 
-                if (texture != lastTexture)
+                if (texture != null && handle != OpenVR.k_ulOverlayHandleInvalid)
                 {
-                    o.SetFullTexture(texture);
-                    lastTexture = texture;
+                    var o = new Utils.OverlayHelper(handle);
+                    o.Show();
+
+                    if (texture != lastTexture)
+                    {
+                        o.SetFullTexture(texture);
+                        lastTexture = texture;
+                    }
+                    o.SetColorWithAlpha(UnityEngine.Color.white);
+                    o.SetWidthInMeters(.05f);
+
+
+
+                    o.SetInputMethod(VROverlayInputMethod.None);
+                    o.SetMouseScale(1, 1);
+
+                    var offset = new SteamVR_Utils.RigidTransform(transform);
+                    if (!Utils.IsFacingHmd(transform))
+                    {
+                        offset.rot = offset.rot * Quaternion.AngleAxis(180, Vector3.up);
+                    }
+
+                    o.SetTransformAbsolute(ETrackingUniverseOrigin.TrackingUniverseStanding, offset);
                 }
-                o.SetColorWithAlpha(UnityEngine.Color.white);
-                o.SetWidthInMeters(.05f);
-
-                
-
-                o.SetInputMethod(VROverlayInputMethod.None);
-                o.SetMouseScale(1, 1);
-
-                var offset = new SteamVR_Utils.RigidTransform(transform);
-                if (!Utils.IsFacingHmd(transform))
-                {
-                    offset.rot = offset.rot * Quaternion.AngleAxis(180, Vector3.up);
-                }
-
-                o.SetTransformAbsolute(ETrackingUniverseOrigin.TrackingUniverseStanding, offset);
             }
         }
 
